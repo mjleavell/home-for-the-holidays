b035bb34146ea137a99a13f254d073cbf58d0ce7
Maggie Leavell <mjleavell@gmail.com>
2018-11-13 21:22:05 -0600

    views are working; hid authentication button bc unnecessary

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import firebase from 'firebase/app';
 import 'bootstrap';
 
 import apiKeys from '../db/apiKeys.json';
+
 import createNavbar from './components/Navbar/navbar';
 import loginButton from './components/Auth/auth';
+import checkLoginStatus from './helpers/authHelpers';
 
 import './index.scss';
 
 const initApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
+  checkLoginStatus();
   loginButton();
 };
 
