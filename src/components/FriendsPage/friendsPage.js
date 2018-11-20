4c96a35848ce27904de70ec253bc66c678f026b5
Maggie Leavell <mjleavell@gmail.com>
2018-11-19 19:15:13 -0600

    friends with correct uid console logging

---
+++ src/components/FriendsPage/friendsPage.js
@@ -0,0 +1,25 @@
+// import $ from 'jquery';
+import axios from 'axios';
+import apiKeys from '../../../db/apiKeys.json';
+import authHelpers from '../../helpers/authHelpers';
+
+const friendsPage = () => {
+  const uid = authHelpers.getCurrentUid();
+  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((results) => {
+      const friendsObject = results.data;
+      const friendsArray = [];
+      if (friendsObject !== null) {
+        Object.keys(friendsObject).forEach((friendId) => {
+          friendsObject[friendId].id = friendId;
+          friendsArray.push(friendsObject[friendId]);
+        });
+      }
+      console.log(friendsArray);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export default friendsPage;
